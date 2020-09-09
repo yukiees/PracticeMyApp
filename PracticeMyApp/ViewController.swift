5f92af37bfba6459a46a409558efdd190b2d9ab1
yukiees <yukiii8989@gmail.com>
2020-09-10 00:48:25 +0900

    Initial Commit

---
+++ PracticeMyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeMyApp
+//
+//  Created by 松島優希 on 2020/09/10.
+//  Copyright © 2020 松島優希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
